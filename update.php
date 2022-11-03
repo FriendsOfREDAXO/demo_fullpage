9443ceaaf979d039f07412d565614401936a8091
aeberhard <andreaseberhard@gmail.com>
2022-11-03 22:05:07 +0100

    update.php angepasst
    Bei Update aus dem Installer muss auch die package.setup.yml gemerged werden

--- update.php
+++ update.php
@@ -1,3 +1,17 @@
 <?php
 
 $addon = rex_addon::get('demo_fullpage');
+
+// update config
+// merge current config with additional config
+
+// Background information:
+// We need the demo to be installed first of all to fetch required packages and import data.
+// To make this happen, we need to keep the config free of external dependencies and use an
+// additional config which will be merged into the config when the demo is installed.
+$config = array_replace_recursive(
+    rex_file::getConfig($addon->getPath('package.yml')),
+    rex_file::getConfig($addon->getPath('package.setup.yml'))
+);
+
+rex_file::putConfig($addon->getPath('package.yml'), $config);
