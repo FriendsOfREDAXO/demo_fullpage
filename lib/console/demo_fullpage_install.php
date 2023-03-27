934b8f2339f9b715c40d53349c19558d304faa2f
aeberhard <andreaseberhard@gmail.com>
2023-03-27 23:36:55 +0200

    php cs fixer

--- lib/console/demo_fullpage_install.php
+++ lib/console/demo_fullpage_install.php
@@ -33,7 +33,7 @@ class rex_command_demo_fullpage_install extends rex_console_command
         $errors = rex_demo_fullpage::install();
 
         if (count($errors) > 0) {
-            $io->error($this->decodeMessage("Failed to install demo:\n- " . join("\n- ", $errors)));
+            $io->error($this->decodeMessage("Failed to install demo:\n- " . implode("\n- ", $errors)));
             return 1;
         }
         $io->success('Successfully installed demo!');
